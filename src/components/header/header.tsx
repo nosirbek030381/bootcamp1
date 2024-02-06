36470bb3ee8ba5d817a6ecd1efc40278edb9125d
WebMoney <ismoilovn899@gmail.com>
2024-02-06 22:42:57 +0500

    #Navbar

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,27 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { AiOutlineSearch, AiOutlineUser } from 'react-icons/ai';
+import { BiBellMinus } from 'react-icons/bi';
 
 const Header = () => {
+	const [scrolled, setScrolled] = useState(false);
+
+	useEffect(() => {
+		const handlerScroll = () => {
+			if (window.scrollY > 0) {
+				setScrolled(true);
+			} else {
+				setScrolled(false);
+			}
+		};
+		window.addEventListener('scroll', handlerScroll);
+
+		return () => window.removeEventListener('scroll', handlerScroll);
+	}, []);
+
 	return (
-		<header>
+		<header className={`${scrolled && 'bg-[#e10856] shadow-lg'}`}>
 			<div className='flex items-center space-x-2 md:space-x-10'>
 				<Image
 					src={'/logo.svg'}
@@ -21,7 +40,14 @@ const Header = () => {
 				</ul>
 			</div>
 
-			<div></div>
+			<div className='flex item-center space-x-4 text-[16px] font-light'>
+				<AiOutlineSearch className='h-6 w-6 cursor-pointer' />
+				<p className='hidden lg:inline'>Kids</p>
+				<BiBellMinus className='h-6 w-6 cursor-pointer' />
+				<Link href={'/account'}>
+					<AiOutlineUser className='h-6 w-6 cursor-pointer' />
+				</Link>
+			</div>
 		</header>
 	);
 };
