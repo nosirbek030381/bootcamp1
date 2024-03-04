c7bb0c22c4c071fa2ef2edcb4bdb8f5741b562d4
nosirbek030381 <ismoilovn899@gmail.com>
2024-03-04 12:19:02 +0500

    Subscription plan

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,5 @@ export { default as Header } from './header/header';
 export { default as Hero } from './hero/hero';
 export { default as Modal } from './modal/modal';
 export { default as Row } from './row/row';
+export { default as Subscription } from './subscription/subscription';
 export { default as TextField } from './text-field/text-field';
