a69b954b3a5c09f61ac90cbb5f4d76d6db45d740
nosirbek030381 <ismoilovn899@gmail.com>
2024-03-20 16:06:53 +0500

    Account UI

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Header } from './header/header';
 export { default as Hero } from './hero/hero';
+export { default as Membership } from './membership/membership';
 export { default as Modal } from './modal/modal';
 export { default as Row } from './row/row';
 export { default as Subscription } from './subscription/subscription';
---
+++ src/components/membership/membership.tsx
@@ -0,0 +1,40 @@
+const Membership = () => {
+	return (
+		<div className='mt-6 grid grid-cols-1 gap-x-4 border px-4 py-4 md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:pb-0'>
+			<div className='space-y-2 py-4'>
+				<h4 className='text-lg text-[gray]'>Membership & Billing</h4>
+
+				<button className='h-10 w-3/5 transition-all whitespace-nowrap bg-gray-300 py-2 text-sm font-medium text-black shadow-md hover:bg-gray-200'>
+					Cancel membership
+				</button>
+			</div>
+			<div className='col-span-3'>
+				<div className='flex flex-col justify-between border-b border-white/10 py-4 md:flex-row'>
+					<div>
+						<p className='font-medium'>info@nosirbek.uz</p>
+						<p className='text-[gray]'>Password: ******</p>
+					</div>
+
+					<div>
+						<p className={'membership'}>Change email</p>
+						<p className={'membership'}>Change password</p>
+					</div>
+				</div>
+
+				<div className='flex flex-col justify-between pt-4 pb-4 md:flex-row md:pb-0'>
+					<div>
+						<p>Your membership plan will end 14 March 2024</p>
+					</div>
+					<div className='md:text-right'>
+						<p className='membership'>Manage payment info</p>
+						<p className='membership'>Add backup payment method</p>
+						<p className='membership'>Billing detail</p>
+						<p className='membership'>Change billing day</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Membership;
