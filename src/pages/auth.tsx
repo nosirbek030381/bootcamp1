d6dbefa0ff52b8ced5fa884b440479e6636e4f89
nosirbek030381 <ismoilovn899@gmail.com>
2024-04-20 23:26:34 +0500

    qwerty

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,7 +1,7 @@
 import { Form, Formik } from 'formik';
 import Head from 'next/head';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { TextField } from 'src/components';
 import { useAuth } from 'src/hooks/useAuth';
